cfb81b66344d7d981fc5b39713d87f4374683461
uddhavgawali21 <146969862+uddhavgawali21@users.noreply.github.com>
2026-03-28 23:55:10 +0530

    Time: 2 ms (90.69%) | Memory: 47.9 MB (49.77%) - LeetSync

--- 02_Array/283-move-zeroes/move-zeroes.java
+++ 02_Array/283-move-zeroes/move-zeroes.java
@@ -27,9 +27,11 @@ class Solution {
          int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
+                if(i!=j){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
+                }
                 j++;
             }
         }
