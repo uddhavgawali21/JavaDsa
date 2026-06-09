64e407d973c515eb1b24eda2f765ee43bd02b6c9
uddhavgawali21 <146969862+uddhavgawali21@users.noreply.github.com>
2026-06-09 22:15:23 +0530

    Time: 3 ms (87.85%) | Memory: 43 MB (92.00%) - LeetSync

---
+++ 02_Array/20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){
+                stack.push(c);
+            
+            }
+            else if(!stack.isEmpty() && ((stack.peek()=='('&&c==')') ||( stack.peek()=='['&&c==']') || (stack.peek()=='{'&&c=='}'))){
+                    stack.pop();
+                
+            }
+
+            else{
+                return false;
+            }
+
+            
+        }
+
+        return stack.isEmpty();
+        
+    }
+}
