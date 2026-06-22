fb4e28615995e573a4bdd72a79b0a963b9259475
uddhavgawali21 <146969862+uddhavgawali21@users.noreply.github.com>
2026-06-22 17:16:43 +0530

    Time: 2 ms (96.67%) | Memory: 43.1 MB (87.41%) - LeetSync

--- 02_Array/1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 02_Array/1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,22 +1,35 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character,Integer> map = new HashMap<>();
+        // Map<Character,Integer> map = new HashMap<>();
 
-        map.put('b',0);
-        map.put('a',0);
-        map.put('l',0);
-        map.put('o',0);
-        map.put('n',0);
+        // map.put('b',0);
+        // map.put('a',0);
+        // map.put('l',0);
+        // map.put('o',0);
+        // map.put('n',0);
+
+        // for(char c : text.toCharArray()){
+        //     if(map.containsKey(c)){
+        //         map.put(c,(map.get(c)+1));
+        //     }
+        // }
+        // map.put('l',map.get('l')/2);
+        // map.put('o',map.get('o')/2);
+
+        // return Collections.min(map.values());
+
+        int b=0,a=0,l=0,o=0,n=0;
 
         for(char c : text.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c,(map.get(c)+1));
-            }
+            if(c=='b') b++;
+            else if(c=='a') a++;
+            else if(c=='l') l++;
+            else if(c=='o') o++;
+            else if(c=='n') n++;
         }
-        map.put('l',map.get('l')/2);
-        map.put('o',map.get('o')/2);
+        return Math.min(b,Math.min(a,Math.min(n,Math.min(o/2,l/2))));
 
-        return Collections.min(map.values());
+
 
 
        
